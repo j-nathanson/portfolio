4d25e86468fed5432b02919d5be47dd2def2e0c1
jacobnathanson <jacobnath93@gmail.com>
2022-02-22 20:30:36 -0500

    profile image

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { Container, Row } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
+import profile from '../assets/images/profile.jpeg'
 
 export default function About() {
     return (
         <Container className='text-center'>
             <Row>
-                <h1 className='text-center'>Hey, I'm Jacob</h1>
+                <h1>Hey, I'm Jacob</h1>
             </Row>
-            <Row>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus iste provident necessitatibus facere tempore sapiente atque quaerat itaque molestiae veniam?</p>
+            <Row className='justify-content-center'>
+                <Col xs={8}>
+                    <img className='profile' src={profile} alt="profile" />
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={10}>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus iste provident necessitatibus facere tempore sapiente atque quaerat itaque molestiae veniam?</p>
+                </Col>
+
             </Row>
         </Container>
     )
