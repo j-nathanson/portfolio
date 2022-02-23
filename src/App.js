c4d25593c73860064237ba8a931717c343fefd4f
jacobnathanson <jacobnath93@gmail.com>
2022-02-22 19:46:29 -0500

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 import ReactFullpage from '@fullpage/react-fullpage';
 
 function App() {
+
+  const anchors = ["firstPage", "secondPage", "thirdPage", "fourthPage"];
+
   return (
     <ReactFullpage
-      scrollingSpeed={1000} /* Options here */
-
+      anchors={anchors}
+      scrollingSpeed={1000}
+      navigation='true'
+      loopBottom='true'
       render={({ state, fullpageApi }) => {
         return (
           <ReactFullpage.Wrapper>
-            <div className="section">
-              <p>Section 1 (welcome to fullpage.js)</p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Click me to move down
-              </button>
+            <div><h1>nav</h1></div>
+            <div className="section s1">
+              <p>about</p>
+
             </div>
-            <div className="section">
-              <p>Section 2</p>
+            <div className="section s2">
+              <p>projects</p>
+
+            </div>
+            <div className="section s3">
+              <p>certifications</p>
+
+            </div>
+            <div className="section s4">
+              <p>contact</p>
+              <button onClick={() => fullpageApi.moveTo('secondPage')}>
+                Click me to move up
+              </button>
             </div>
           </ReactFullpage.Wrapper>
         );
