7230e967d2184fd15f078512fcfa18315c355923
jacobnathanson <jacobnath93@gmail.com>
2022-02-24 16:29:18 -0500

    used scss and added transition effects

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ import profile from '../assets/images/profile.jpeg'
 
 export default function About() {
     return (
-        <Container className='content-container d-flex flex-column justify-content-around'>
+        <Container className='content-container d-flex flex-column justify-content-around p-3'>
             <Row className=''>
-                <h1 className='display-1 text-center'>Hey, I'm Jacob</h1>
+                <h1 className='text-center'>Hey, I'm Jacob</h1>
             </Row>
             <Row className='justify-content-center text-center'>
                 <Col>
@@ -14,8 +14,8 @@ export default function About() {
                 </Col>
             </Row>
             <Row className='justify-content-center '>
-                <Col xs={10} lg={6}>
-                    <p>A Full stack web and mobile app developer from Brooklyn, NY.I am a graduate of NuCamp's Full stack bootcamp and I love using React, Redux toolkit, and Sass to make my project come alive!</p>
+                <Col xs={10} lg={8}>
+                    <p>A Full stack web and mobile app developer from Brooklyn, NY. I am a graduate of NuCamp's Full stack bootcamp and I love using React, Redux toolkit, and Scss to make my project come alive!</p>
                 </Col>
             </Row>
         </Container>
